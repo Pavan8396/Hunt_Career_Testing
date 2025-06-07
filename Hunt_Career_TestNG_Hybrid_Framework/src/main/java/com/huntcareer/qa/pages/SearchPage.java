cb0d390b707c08a87997b6d91be6be20fbc3c3c8
Pavan <pavanumashankar8@gmail.com>
2025-06-07 22:56:33 +0530

    Search Test Scripts

---
+++ Hunt_Career_TestNG_Hybrid_Framework/src/main/java/com/huntcareer/qa/pages/SearchPage.java
@@ -0,0 +1,107 @@
+package com.huntcareer.qa.pages;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SearchPage {
+	WebDriver driver;
+	WebDriverWait wait;
+
+	@FindBy(xpath = "//input[@placeholder='Search jobs by title, company, or keyword...']")
+	private WebElement SearchInputField;
+
+	@FindBy(xpath = "//button[@aria-label='Search']")
+	private WebElement SearchButton;
+
+	@FindBy(xpath = "//button[@aria-label='Toggle location filter dropdown']")
+	private WebElement LocationButton;
+
+	@FindBy(xpath = "//input[@placeholder='Search location...']")
+	private WebElement LocationSearchInput;
+
+	@FindBy(xpath = "//button[@aria-label='Toggle job type filter dropdown']")
+	private WebElement JobTypeButton;
+
+	@FindBy(xpath = "//input[@placeholder='Search job type...']")
+	private WebElement JobTypeSearchInput;
+
+	@FindBy(xpath = "//button[@aria-label='Clear all filters']")
+	private WebElement ClearAllFiltersButton;
+	
+	@FindBy(xpath = "//p[contains(text(),'No jobs found.')]")
+	private WebElement noJobsFoundMessage;
+
+	public SearchPage(WebDriver driver) {
+		this.driver = driver;
+		this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		PageFactory.initElements(driver, this);
+	}
+	
+	public void enterKeywordIntoSearchInputField(String searchTerm) {
+		SearchInputField.sendKeys(searchTerm);
+	}
+	
+	public void clickOnSearch() {
+		SearchButton.click();
+	}
+	
+	public void clickLocationButton() {
+		LocationButton.click();
+	}
+	
+	public void enterLocationSearchInput(String locationText) {
+		LocationSearchInput.sendKeys(locationText);
+	}
+	
+	public void selectLocationOption(String location) {
+		driver.findElement(By.xpath("//label[contains(., '" + location + "')]/input")).click();
+	}
+	
+	public void clickJobTypeButton() {
+		JobTypeButton.click();
+	}
+	
+	public void enterJobTypeSearchInput(String jobTypeText) {
+		JobTypeSearchInput.sendKeys(jobTypeText);
+	}
+	
+	public void selectJobTypeOption(String jobType) {
+		driver.findElement(By.xpath("//label[contains(., '" + jobType + "')]/input")).click();
+	}
+	
+	public void clickClearFilterOption() {
+		ClearAllFiltersButton.click();
+	}
+
+	public WebElement getJobCardTitle(WebElement jobcard) {
+		return jobcard.findElement(By.cssSelector("h3.text-lg.font-semibold"));
+	}
+
+	public WebElement jobCardCompanyLocation(WebElement jobcard) {
+		return jobcard.findElement(By.cssSelector("p.text-gray-600"));
+	}
+
+	public WebElement jobCardDescription(WebElement jobcard) {
+		return jobcard.findElement(By.cssSelector("p.text-sm.text-gray-700.mt-3.line-clamp-3"));
+	}
+	
+	public void getSearchTag(String searchTerm) {
+        driver.findElement(By.xpath("//span[contains(text(), " + searchTerm + ")]")).isDisplayed();
+    }
+
+    public void getLocationTag(String location) {
+        driver.findElement(By.xpath("//span[contains(text(), " + location + ")]")).isDisplayed();
+    }
+
+    public void getJobTypeTag(String jobType) {
+        driver.findElement(By.xpath("//span[contains(text(), " + jobType + ")]")).isDisplayed();
+    }
+    
+    
+}
---
+++ Hunt_Career_TestNG_Hybrid_Framework/src/test/java/com/huntcareer/qa/testcases/Search.java
@@ -0,0 +1,70 @@
+package com.huntcareer.qa.testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.huntcareer.qa.base.Base;
+import com.huntcareer.qa.pages.SearchPage;
+
+public class Search extends Base {
+	WebDriver driver;
+	SearchPage sp;
+
+	public Search() {
+		super();
+	}
+
+	@BeforeMethod
+	public void setup() {
+		// loadPropertiesFile();
+		driver = inicializeBrowserAndOpenApplication(prop.getProperty("browser"));
+		// driver.get(loadPropertiesFile().getProperty("url")+"/signup");
+		driver.get(prop.getProperty("url"));
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	@Test(priority = 1)
+	public void TC_1_verifyValidSearch() {
+
+		sp = new SearchPage(driver);
+		sp.enterKeywordIntoSearchInputField(dataProp.getProperty("validSearchTerm"));
+		sp.clickOnSearch();
+		sp.getSearchTag("validSearchTerm");
+	}
+	
+	@Test(priority = 2)
+	public void TC_2_verifyInvalidSearch() {
+		sp = new SearchPage(driver);
+		sp.enterKeywordIntoSearchInputField(dataProp.getProperty("invalidSearchTerm"));
+		sp.clickOnSearch();
+		sp.getSearchTag("invalidSearchTerm");
+	}
+	
+	@Test(priority = 3)
+	public void TC_3_verifySearchWithSpecialCharcters() {
+		sp = new SearchPage(driver);
+		sp.enterKeywordIntoSearchInputField(dataProp.getProperty("specialCharctersSearchTerm"));
+		sp.clickOnSearch();
+		sp.getSearchTag("specialCharctersSearchTerm");
+	}
+	
+	@Test(priority = 4)
+	public void TC_4_verifySearchWithFilters() {
+		sp = new SearchPage(driver);
+		sp.enterKeywordIntoSearchInputField(dataProp.getProperty("validSearchTerm"));
+		sp.clickLocationButton();
+		sp.selectLocationOption(dataProp.getProperty("filterLocation"));
+		sp.clickLocationButton();
+		sp.clickJobTypeButton();
+		sp.selectJobTypeOption(dataProp.getProperty("filterJobType"));
+		sp.getSearchTag("validSearchTerm");
+		sp.getLocationTag("filterLocation");
+		sp.getJobTypeTag("filterJobType");
+	}
+}
