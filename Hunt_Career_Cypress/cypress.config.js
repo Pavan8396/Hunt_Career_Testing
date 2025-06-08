ccacc8c7af870e387d8b14e32d9ede843f2d5504
Pavan <pavanumashankar8@gmail.com>
2025-06-08 21:43:10 +0530

    updated config file

--- Hunt_Career_Cypress/cypress.config.js
+++ Hunt_Career_Cypress/cypress.config.js
@@ -1,25 +1,51 @@
+// const { defineConfig } = require("cypress");
+
+// module.exports = defineConfig({
+//   e2e: {
+//     baseUrl: 'http://localhost:3000',
+//     setupNodeEvents(on, config) {
+//       require('cypress-mochawesome-reporter/plugin')(on);
+//     },
+//     reporter: 'cypress-mochawesome-reporter',
+//     reporterOptions: {
+//       charts: true,
+//       reportPageTitle: 'Test Report',
+//       embeddedScreenshots: true,
+//       inlineAssets: true,
+//       saveAllAttempts: true,
+//       overwrite: false,
+//       html: false,
+//       json: true
+//     },
+//     // retries: {
+//     //   runMode: 2,
+//     //   openMode: 0,
+//     // },
+//   },
+// });
+
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  baseUrl: 'http://localhost:3000',
+
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'mochawesome-report',
+    charts: true,
+    reportPageTitle: 'Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: true,
+    overwrite: false,
+    html: false, // Generate only JSON for merging
+    json: true
+  },
+
   e2e: {
-    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
-    },
-    reporter: 'cypress-mochawesome-reporter',
-    reporterOptions: {
-      charts: true,
-      reportPageTitle: 'Test Report',
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      saveAllAttempts: true,
-      overwrite: false,
-      html: false,
-      json: true
-    },
-    // retries: {
-    //   runMode: 2,
-    //   openMode: 0,
-    // },
-  },
+      return config;
+    }
+  }
 });
