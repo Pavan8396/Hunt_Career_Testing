b381801198d856a5790ce8a08eabeb04320367fb
Pavan <pavanumashankar8@gmail.com>
2025-06-08 21:29:55 +0530

    updated config file

--- Hunt_Career_Cypress/cypress.config.js
+++ Hunt_Career_Cypress/cypress.config.js
@@ -1,22 +1,31 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  baseUrl: 'http://localhost:3000',
+
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir: 'mochawesome-report',
+    charts: true,
+    reportPageTitle: 'Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: true,
+    overwrite: false,
+    html: false,
+    json: true
+  },
+
   e2e: {
-    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
+      return config;
     },
-    reporter: 'cypress-mochawesome-reporter',
-    reporterOptions: {
-      charts: true,
-      reportPageTitle: 'Test Report',
-      embeddedScreenshots: true,
-      inlineAssets: true,
-      saveAllAttempts: true,
-    },
-    // retries: {
-    //   runMode: 2,
-    //   openMode: 0,
-    // },
   },
+
+  // Optional: Enable retries
+  // retries: {
+  //   runMode: 2,
+  //   openMode: 0,
+  // },
 });
